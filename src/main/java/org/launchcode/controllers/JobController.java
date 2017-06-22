ac1265beca2636edd3c9310ce64036cb286701f6
Lonny Snyder <lonnysnyder81@gmail.com>
2017-06-22 13:20:14 -0500

    added feature to pass new job to view from controller

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -24,6 +25,8 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
+        Job job=jobData.findById(id);
+        model.addAttribute(job);
 
         return "job-detail";
     }
@@ -40,6 +43,9 @@ public class JobController {
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
+        if (errors.hasErrors()){
+            
+        }
 
         return "";
 
